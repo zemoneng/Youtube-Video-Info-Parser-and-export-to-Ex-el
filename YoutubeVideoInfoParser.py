d9ee2b73264f7b9db6aebf7a046880b741c72364
Zemoneng <zemoneng@users.noreply.github.com>
2021-04-14 14:41:22 +0300

    - added english comments - moving constants to the top of the code

--- YoutubeVideoInfoParser.py
+++ YoutubeVideoInfoParser.py
@@ -4,18 +4,19 @@
 import random
 import time
 
-import pandas as pd  # для создания Excel-таблицы в формате .xlsx
-import requests as req  # для обхода ссылок
-from bs4 import BeautifulSoup
+import requests as req  # need for download pages
+from bs4 import BeautifulSoup  # need for grab markup from Youtube pages
+import pandas as pd  # need for create Excel spreadsheet (.xlsx file)
 
-# Add your Youtube URL's in list_of_urls to grab information about videos
-# Добавь сюда урлы, с которых хочешь собрать информацию:
+# Add your Youtube URL's below in list_of_urls to grab information about videos:
 list_of_urls = ["https://www.youtube.com/watch?v=7I6EyEonVLQ&list=PL66DIGaegedrOcVcDyE8zfSqKgpToMOeM&index=1",
                 "https://www.youtube.com/watch?v=D25Btx8srSU&list=PL66DIGaegedrOcVcDyE8zfSqKgpToMOeM&index=2",
                 "https://www.youtube.com/watch?v=ZbwqquA7rxE&list=PL66DIGaegedrOcVcDyE8zfSqKgpToMOeM&index=3"]
 
+WAITING_TIME = 2  # set number of seconds waiting until grab next URL (to avoid Youtube blocking)
 
-# Парсер одной страницы
+
+# Page parser
 def info_parser(url_):
     resp = req.get(url_)
     soup = BeautifulSoup(resp.text, 'lxml')
@@ -32,25 +33,27 @@ Views_list = []
 UploadDate_list = []
 
 
-# Последовательно парсит все урлы из списка list_of_urls и формирует будущие колонки с данными
-for i in list_of_urls:
-    current_video_info = info_parser(i)
-    print(current_video_info)  # проверка работает ли парсер
+# Sequentially runs URLs from list_of_urls and writes the data to the lists
+for url in list_of_urls:
+    current_video_info = info_parser(url)
+    print(current_video_info)  # prints the collected data
 
     Name_list.append(current_video_info[1])
     Views_list.append(int(current_video_info[2]))
     UploadDate_list.append(current_video_info[3])
-    time.sleep(random.uniform(0, 2))  # рандомная задержка между обходом страниц, чтобы ютьюб не забанил
+    time.sleep(random.uniform(0, WAITING_TIME))  # set random delay to avoid Youtube blocking
 
-Current_time = time.strftime("%m-%d-%Y_%Hh%M_min")  # время завершения сбора данных
+# time of completed data collection
+Current_time = time.strftime("%m-%d-%Y_%Hh%Mm")
 
-# Формирует Excel-файл с данными в виде таблицы
+
+# Creating an Excel spreadsheet
 table_from_youtube_list = pd.DataFrame({'URL': list_of_urls,
                                         'Name': Name_list,
                                         'Views': Views_list,
                                         'UploadDate': UploadDate_list
                                         })
-table_from_youtube_list.index += 1  # индекс в таблице начинается с 1 вместо 0
+table_from_youtube_list.index += 1  # index in the table starts with 1 instead of 0
 
-# создает файл Excel в папке проекта с указанием даты обхода парсером в имени файла:
-table_from_youtube_list.to_excel('./' + Current_time + '_YoutubeVideoInfo.xlsx', sheet_name=Current_time)
+# creates the resulting Excel file in the project folder
+table_from_youtube_list.to_excel('./' + "result_" + Current_time + '_YoutubeVideoInfo.xlsx', sheet_name=Current_time)
